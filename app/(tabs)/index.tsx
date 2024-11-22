ca40b19274d6262ba0759a9271f9f053bb4ac51a
tristan sacotte <tristansacotte@tristans-MBP.lan>
2024-11-21 21:33:40 -0600

    added explorer header

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,22 @@
 import { View, Text } from "react-native";
-import React from "react";
-import { Link } from "expo-router";
+import React, { useState } from "react";
+import { Link, Stack } from "expo-router";
+import ExploreHeader from "@/components/ExploreHeader";
+import Listings from "@/components/Listings";
 
 const Page = () => {
+  const [category, setCategory] = useState("Tiny homes");
+  const onDataChange = (category: string) => {
+    setCategory(category);
+  };
   return (
-    <View>
-      <Link href={"/(modals)/login"}>Login</Link>
-      <Link href={"/(modals)/booking"}>Booking</Link>
-      <Link href={"/listing/1337"}>Listing details</Link>
+    <View style={{ flex: 1, marginTop: 130 }}>
+      <Stack.Screen
+        options={{
+          header: () => <ExploreHeader onCategoryChange={onDataChange} />,
+        }}
+      />
+      <Listings listings={[]} category={category} />
     </View>
   );
 };
